45c2e61d81fd1b091fe844f71813f36215747c4e
Michael Friedrich <mifrie@microsoft.com>
2017-08-08 15:28:38 +1000

    hopeful

--- downloadhugo.sh
+++ downloadhugo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #download and install hugo 0.26
-curl https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Linux-64bit.tar.gz | tar xvz
+curl -fSL https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Linux-64bit.tar.gz -o hugo.tar.gz
+tar -xvzf hugo.tar.gz
 
 #verify install
 hugo version
