d762cf2c2aba627ee638c8044c5da4fd07ba401d
Michael Friedrich <mifrie@microsoft.com>
2017-08-08 15:20:03 +1000

    fixed install script

--- downloadhugo.sh
+++ downloadhugo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #download and install hugo 0.26
-curl -O https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Linux-64bit.deb
-sudo dpkg -i hugo*.deb
+curl -O https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Linux-64bit.tar.gz
+tar -xvzf hugo_0.26_Linux-64bit.tar.gz
 
 #verify install
 hugo version
