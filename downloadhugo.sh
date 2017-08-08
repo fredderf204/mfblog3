4494a11b7e890840ed9d72c114bcd2be0487f201
Michael Friedrich <mifrie@microsoft.com>
2017-08-08 16:43:32 +1000

    script test

--- downloadhugo.sh
+++ downloadhugo.sh
@@ -2,6 +2,7 @@
 #download and install hugo 0.26
 curl -fSL https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Linux-64bit.tar.gz -o hugo.tar.gz
 tar -xvzf hugo.tar.gz
+sudo cp hugo /usr/local/bin
 
 #verify install
 hugo version
