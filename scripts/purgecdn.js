c79e7ee6fcb3c59c7eeaabecd106399c51c3490f
Michael Friedrich <mifrie@microsoft.com>
2018-03-30 12:05:42 +1100

    added packages

--- scripts/purgecdn.js
+++ scripts/purgecdn.js
@@ -1,18 +1,18 @@
 var CDNManagementClient = require('azure-arm-cdn');
 var MsRest = require('ms-rest-azure');
-var subscriptionId = env.AZURE_SUBSCRIPTION_ID;
+var subscriptionId = process.env.AZURE_SUBSCRIPTION_ID;
 
 MsRest.loginWithServicePrincipalSecret(
-    env.AZURE_CLIENT_ID,
-    env.AZURE_CLIENT_SECRET,
-    env.AZURE_TENANT_ID,
+    process.env.AZURE_CLIENT_ID,
+    process.env.AZURE_CLIENT_SECRET,
+    process.env.AZURE_TENANT_ID,
     (err, credentials) => {
         if (err) throw err
         let client = new CDNManagementClient(credentials, subscriptionId);
         var purgeContentPaths = [
             '/*'
         ]
-        client.endpoints.purgeContent(env.azurerg, env.cdnprofile, env.cdnendpoint, purgeContentPaths, function (err, result, request, response) {
+        client.endpoints.purgeContent(process.env.azurerg, process.env.cdnprofile, process.env.cdnendpoint, purgeContentPaths, function (err, result, request, response) {
             if (err) { 
                 console.log(err); 
             } else {
