cb82c86f61daed11f85c39f9648fab293b3964f0
Michael Friedrich <mifrie@microsoft.com>
2018-10-09 17:08:09 +1100

    hello

--- buildscripts/blobupload.js
+++
@@ -1,9 +0,0 @@
-const mime = require('mime')
-const blobService = require('azure-storage');
-const fs = require('fs');
-const path = require('path')
-const upFolder = path.normalize(process.env.WORKSPACE + "\\public");
-
-fs.readdirSync(upFolder).forEach(file => {
-  console.log(file);
-})
